ac3bb1a199cd69ba66a8b42446bf48ab915330f3
Leon <beckerleon250@gmail.com>
2023-01-07 22:22:01 +0100

    Added hammers

---
+++ Scripts/Firearms/FirearmMechanics/Hammer.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+using ThunderRoad;
+
+namespace GhettosFirearmSDKv2
+{
+    [AddComponentMenu("Firearm SDK v2/Firearm components/Hammer")]
+    public class Hammer : MonoBehaviour
+    {
+        public Item item;
+        public Transform hammer;
+        public Transform idlePosition;
+        public Transform cockedPosition;
+        public List<AudioSource> hitSounds;
+
+        private void Awake()
+        {
+            hammer.localPosition = cockedPosition.localPosition;
+            hammer.localEulerAngles = cockedPosition.localEulerAngles;
+        }
+
+        public void Pull()
+        {
+            hammer.localPosition = cockedPosition.localPosition;
+            hammer.localEulerAngles = cockedPosition.localEulerAngles;
+        }
+
+        public void Fire(bool silent = false)
+        {
+            hammer.localPosition = idlePosition.localPosition;
+            hammer.localEulerAngles = idlePosition.localEulerAngles;
+            //if (!silent) Util.PlayRandomAudioSource(hitSounds);
+        }
+    }
+}
