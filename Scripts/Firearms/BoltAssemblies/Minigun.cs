fcb403db2d881fe8614d0cf88f31c207420eb812
Leon Becker <beckerleon250@gmail.com>
2024-03-25 22:01:32 +0100

    Update

--- Scripts/Firearms/BoltAssemblies/Minigun.cs
+++ Scripts/Firearms/BoltAssemblies/Minigun.cs
@@ -7,6 +7,9 @@ namespace GhettosFirearmSDKv2
     [AddComponentMenu("Firearm SDK v2/Bolt assemblies/Minigun")]
     public class Minigun : BoltBase
     {
+        public bool revOnTrigger;
+        public bool loopingMuzzleFlash;
+        
         public float[] barrelAngles;
         public Transform roundMount;
         public Cartridge loadedCartridge;
